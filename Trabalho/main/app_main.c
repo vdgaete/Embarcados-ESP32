d6ec30b2459d49aa92b5e632192dc50e856c9541
vdgaete <vdgaete@hotmail.com>
2022-12-19 18:39:25 -0300

    aa

---
+++ Trabalho/main/app_main.c
@@ -0,0 +1,443 @@
+/* MQTT (over TCP) Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
+
+// Includes do ADC
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
+#include "freertos/semphr.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "driver/timer.h"
+
+//ADC Channels
+#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6
+#define ADC2_EXAMPLE_CHAN0          ADC2_CHANNEL_0
+static const char *TAG_CH[2][10] = {{"ADC1_CH6"}, {"ADC2_CH0"}};
+//ADC Attenuation
+#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
+//ADC Calibration
+#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
+
+//GPIO
+#define GPIO_OUTPUT_IO_2    2
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_2))
+#define GPIO_INPUT_IO_0     0
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_0))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+//Timer
+#define TIMER_DIVIDER         (16)  //  Hardware timer clock divider
+#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
+#define TIMER_MQTT            (60)  // MQTT atualiza aós esse tempo
+
+//Var global
+int voltage = 1;
+int voltage1 = 1;
+static int adc_raw[2][10];
+static const char *TAG_ADC = "ADC SINGLE";
+//esp_mqtt_client_handle_t client;
+
+static esp_adc_cal_characteristics_t adc1_chars;
+static esp_adc_cal_characteristics_t adc2_chars;
+
+typedef struct {
+    int timer_group;
+    int timer_idx;
+    int alarm_interval;
+    int counter_dir;
+    bool auto_reload;
+} example_timer_info_t;
+
+typedef struct {
+    example_timer_info_t info;
+    uint64_t timer_counter_value;
+} example_timer_event_t;
+
+static xQueueHandle s_timer_queue;
+
+static xQueueHandle gpio_evt_queue = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg; 
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            
+        //FAZ A LEITURA DO ADC E ATUALIZA A VARIAVEL VOLTAGE
+        adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0);
+        ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
+        if (cali_enable) {
+            voltage = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
+            ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage);
+        }
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    
+        do {
+            ret = adc2_get_raw(ADC2_EXAMPLE_CHAN0, ADC_WIDTH_BIT_DEFAULT, &adc_raw[1][0]);
+        } while (ret == ESP_ERR_INVALID_STATE);
+        ESP_ERROR_CHECK(ret);
+
+        ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
+        if (cali_enable) {
+            voltage = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc2_chars);
+            ESP_LOGI(TAG_CH[1][0], "cali data: %d mV", voltage);
+        }
+        ////////////////////////////////////////////////////////////////
+
+        char buff[32];
+        itoa(voltage,buff,10);
+        //esp_mqtt_client_publish(client,"vbat1",buff, 0, 1 ,0 );
+        printf("\n Aqui a gente publica no Broker!!! \n");
+        timer_set_counter_value(TIMER_GROUP_0,TIMER_0,0);
+        }
+    }
+}
+
+static void timer_task_example(void* arg)
+{
+    //FAZ A LEITURA DO ADC E ATUALIZA A VARIAVEL VOLTAGE
+    adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0);
+    ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
+    if (cali_enable) {
+        voltage = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
+        ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage);
+    }
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    
+    do {
+        ret = adc2_get_raw(ADC2_EXAMPLE_CHAN0, ADC_WIDTH_BIT_DEFAULT, &adc_raw[1][0]);
+    } while (ret == ESP_ERR_INVALID_STATE);
+    ESP_ERROR_CHECK(ret);
+
+    ESP_LOGI(TAG_CH[1][0], "raw  data: %d", adc_raw[1][0]);
+    if (cali_enable) {
+        voltage = esp_adc_cal_raw_to_voltage(adc_raw[1][0], &adc2_chars);
+        ESP_LOGI(TAG_CH[1][0], "cali data: %d mV", voltage);
+    }
+        ////////////////////////////////////////////////////////////////
+
+    char buff[32];
+    itoa(voltage,buff,10);
+    //esp_mqtt_client_publish(client,"vbat1",buff, 0, 1 ,0 );
+    printf("\n Aqui a gente publica no Broker!!! \n");
+}
+
+static void inline print_timer_counter(uint64_t counter_value)
+{
+    printf("Counter: 0x%08x%08x\r\n", (uint32_t) (counter_value >> 32),
+           (uint32_t) (counter_value));
+    printf("Time   : %.8f s\r\n", (double) counter_value / TIMER_SCALE);
+}
+
+static bool IRAM_ATTR timer_group_isr_callback(void *args)
+{
+    BaseType_t high_task_awoken = pdFALSE;
+    example_timer_info_t *info = (example_timer_info_t *) args;
+
+    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(info->timer_group, info->timer_idx);
+
+    /* Prepare basic event data that will be then sent back to task */
+    example_timer_event_t evt = {
+        .info.timer_group = info->timer_group,
+        .info.timer_idx = info->timer_idx,
+        .info.auto_reload = info->auto_reload,
+        .info.alarm_interval = info->alarm_interval,
+        .info.counter_dir = info->counter_dir,
+        .timer_counter_value = timer_counter_value
+    };
+
+    if (!info->auto_reload) {
+        if(!info->counter_dir){
+            timer_counter_value -= info->alarm_interval * TIMER_SCALE;
+        }
+        else{
+            timer_counter_value += info->alarm_interval * TIMER_SCALE;
+        }
+        timer_group_set_alarm_value_in_isr(info->timer_group, info->timer_idx, timer_counter_value);
+    }
+
+    /* Now just send the event data back to the main program task */
+    xQueueSendFromISR(s_timer_queue, &evt, &high_task_awoken);
+
+    return high_task_awoken == pdTRUE; // return whether we need to yield at the end of ISR
+}
+
+static void example_tg_timer_init(int group, int timer, bool auto_reload, int timer_interval_sec, int COUNTER_DIR)
+{
+    /* Select and initialize basic parameters of the timer */
+    timer_config_t config = {
+        .divider = TIMER_DIVIDER,
+        .counter_dir = COUNTER_DIR,
+        .counter_en = TIMER_PAUSE,
+        .alarm_en = TIMER_ALARM_EN,
+        .auto_reload = auto_reload,
+    }; // default clock source is APB
+    timer_init(group, timer, &config);
+
+    /* Timer's counter will initially start from value below.
+       Also, if auto_reload is set, this value will be automatically reload on alarm */
+    timer_set_counter_value(group, timer, 0);
+
+    /* Configure the alarm value and the interrupt on alarm. */
+    timer_set_alarm_value(group, timer, timer_interval_sec * TIMER_SCALE);
+
+    /* Configura Timer com direção de contagem para baixo (TIMER_COUNT_DOWN) */
+    if(!COUNTER_DIR){
+        timer_set_counter_value(group, timer, 300 * TIMER_SCALE);
+        timer_set_alarm_value(group, timer, (300 - timer_interval_sec) * TIMER_SCALE);
+    }
+
+    timer_enable_intr(group, timer);
+
+    example_timer_info_t *timer_info = calloc(1, sizeof(example_timer_info_t));
+    timer_info->timer_group = group;
+    timer_info->timer_idx = timer;
+    timer_info->auto_reload = auto_reload;
+    timer_info->alarm_interval = timer_interval_sec;
+    timer_info->counter_dir = COUNTER_DIR;
+    timer_isr_callback_add(group, timer, timer_group_isr_callback, timer_info, 0);
+
+    timer_start(group, timer);
+}
+
+static bool adc_calibration_init(void)
+{
+    esp_err_t ret;
+    bool cali_enable = false;
+
+    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
+    if (ret == ESP_ERR_NOT_SUPPORTED) {
+        ESP_LOGW(TAG_ADC, "Calibration scheme not supported, skip software calibration");
+    } else if (ret == ESP_ERR_INVALID_VERSION) {
+        ESP_LOGW(TAG_ADC, "eFuse not burnt, skip software calibration");
+    } else if (ret == ESP_OK) {
+        cali_enable = true;
+        esp_adc_cal_characterize(ADC_UNIT_1, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
+        esp_adc_cal_characterize(ADC_UNIT_2, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &adc2_chars);
+    } else {
+        ESP_LOGE(TAG_ADC, "Invalid arg");
+    }
+
+    return cali_enable;
+}
+
+static const char *TAG = "MQTT_EXAMPLE";
+
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0) {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+/*
+ * @brief Event handler registered to receive MQTT events
+ *
+ *  This function is called by the MQTT client event loop.
+ *
+ * @param handler_args user data registered to the event.
+ * @param base Event base for the handler(always MQTT Base in this example).
+ * @param event_id The id for the received event.
+ * @param event_data The data for the event, esp_mqtt_event_handle_t.
+ */
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    char buff[32];
+    switch ((esp_mqtt_event_id_t)event_id) {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        /*msg_id = esp_mqtt_client_publish(client, "vbat", "data_3", 0, 1, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);*/
+        
+        msg_id = esp_mqtt_client_subscribe(client, "vbat", 0);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        /*msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+
+        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);*/
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        //msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        //ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        //ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        itoa(voltage,buff,10);
+        msg_id = esp_mqtt_client_publish(client, "vbat1",buff , 0, 0, 0);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start(void)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {
+    .uri = "mqtt://device_2:device_2@node02.myqtthub.com:1883",//CONFIG_BROKER_URL
+    .client_id="device_2",
+    };
+   esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(client);
+    
+}
+
+void app_main(void)
+{   
+    esp_err_t ret = ESP_OK;
+    
+    bool cali_enable = adc_calibration_init();
+
+    //ADC1 config
+    ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
+    //ADC2 config
+    ESP_ERROR_CHECK(adc2_config_channel_atten(ADC2_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
+
+    s_timer_queue = xQueueCreate(10, sizeof(example_timer_event_t));
+    xTaskCreate(timer_task_example, "timer_task_example", 2048, NULL, 10, NULL);
+
+    example_tg_timer_init(TIMER_GROUP_0, TIMER_0, true, TIMER_MQTT, TIMER_COUNT_UP);
+    //example_tg_timer_init(TIMER_GROUP_1, TIMER_0, false, 5, TIMER_COUNT_UP);
+    //example_tg_timer_init(TIMER_GROUP_1, TIMER_1, false, 7, TIMER_COUNT_DOWN);
+
+     //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    mqtt_app_start();
+}
